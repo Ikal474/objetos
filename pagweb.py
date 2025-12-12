e6e3edd98388bd5f5da1afdfc0dc120ae47aab3a
Ikal474 <andres4482@es.uazuay.edu.ec>
2025-12-12 13:21:00 -0500

    Update pagweb.py

--- pagweb.py
+++ pagweb.py
@@ -13,7 +13,7 @@ def load_model():
 
 model = load_model()
 
-CLASSES = ["Licecia", "billetes", "llaves"] 
+CLASSES = ["Tijeras", "billetes", "llaves"] 
 
 def preprocess(img):
     
@@ -50,6 +50,7 @@ if uploaded_file is not None:
 
 #cd "C:\Users\Usuario\OneDrive\Desktop\Universidad\Noveno semestre\IA"
 #streamlit run pagweb.py
+
 
 
 
